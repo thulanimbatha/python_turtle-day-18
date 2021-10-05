f6d3d09195d7b3b9839fb87bf86c916af1213da9
thulanimbatha <thulanimbatha06@gmail.com>
2021-10-05 12:40:46 +0200

    completed challenge 2

--- main.py
+++ main.py
@@ -9,7 +9,14 @@ for _ in range(0,4):
     tmnt.forward(200)   # move Turtle 200 paces
     tmnt.right(90)  # turn turtle right 90 degrees
 
-
+'''Dotted square'''
+for _ in range(0, 4):
+    tmnt.left(90)
+    tmnt.forward(30)
+    tmnt.penup()
+    tmnt.forward(30)
+    tmnt.pendown()
+    tmnt.forward(30)
 
 
 
