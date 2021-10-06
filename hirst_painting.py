b8bfcc8dccff9512b4c40c374e40d5ef60b9fdc6
thulanimbatha <thulanimbatha06@gmail.com>
2021-10-06 13:37:28 +0200

    extracted colours

--- hirst_painting.py
+++ hirst_painting.py
@@ -1,6 +1,14 @@
 # Day 18 project
 import colorgram
 
-colours = colorgram.extract("hirst.jpg", 10)
+rgb_colours = []
+colours = colorgram.extract("hirst.jpg", 30)
 
-print(colours)
+for colour in colours:
+    r = colour.rgb.r
+    g = colour.rgb.g
+    b = colour.rgb.b
+    new_colour = (r, g, b)
+    rgb_colours.append(new_colour)
+
+print(rgb_colours)
