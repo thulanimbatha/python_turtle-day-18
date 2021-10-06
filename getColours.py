3d8329110c6a82ed05569e49ef08fec1d6c47441
thulanimbatha <thulanimbatha06@gmail.com>
2021-10-06 14:17:37 +0200

    added getColours file and completed Hirst Painting project

---
+++ getColours.py
@@ -0,0 +1,13 @@
+import colorgram
+
+colours = colorgram.extract("hirst.jpg", 144)
+rgb_colours = []
+
+for colour in colours:
+    r = colour.rgb.r
+    g = colour.rgb.g
+    b = colour.rgb.b
+    new_colour = (r, g, b)
+    rgb_colours.append(new_colour)
+
+print(rgb_colours)
--- hirst_painting.py
+++ hirst_painting.py
@@ -1,14 +1,30 @@
 # Day 18 project
-import colorgram
+import turtle as t
+import random
 
-rgb_colours = []
-colours = colorgram.extract("hirst.jpg", 30)
+tmnt = t.Turtle()
+t.colormode(255)
+colour_list = [(252, 250, 235), (241, 252, 245), (253, 245, 249), (232, 243, 249), (238, 231, 79), (180, 16, 43), (219, 64, 99), (109, 179, 204), (185, 74, 38), (25, 122, 166), (186, 42, 67), (207, 152, 102), (22, 136, 85), (188, 176, 23), (20, 31, 68), (216, 130, 154), (70, 170, 100), (19, 166, 208), (232, 230, 6), (217, 78, 55), (38, 46, 112), (127, 184, 138), (8, 51, 33), (235, 165, 182), (149, 210, 220), (163, 24, 20), (10, 98, 55), (156, 213, 189), (237, 171, 163), (87, 21, 61), (6, 87, 107), (85, 12, 8), (115, 118, 151), (174, 184, 217), (248, 11, 27)]
+tmnt.speed("fastest")
+tmnt.setheading(230)
+tmnt.penup()
+tmnt.forward(350)
+tmnt.setheading(0)
 
-for colour in colours:
-    r = colour.rgb.r
-    g = colour.rgb.g
-    b = colour.rgb.b
-    new_colour = (r, g, b)
-    rgb_colours.append(new_colour)
+for _ in range(10):
+    for _ in range(10):
+        tmnt.dot(20, random.choice(colour_list))
+        tmnt.forward(50)
 
-print(rgb_colours)
+    tmnt.setheading(90)
+    tmnt.forward(50)
+    tmnt.setheading(180)
+    tmnt.forward(500)
+    tmnt.setheading(0)
+
+# for _ in range(10):
+#     tmnt.dot(20, random.choice(colour_list))
+#     tmnt.forward(50)
+
+screen = t.Screen()
+screen.exitonclick()
